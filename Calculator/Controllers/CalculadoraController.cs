86f86f69e8fc85691db37cdf7c72e3e958987759
Mikhael Pedro <mikhaelpedro2000@gmail.com>
2021-09-03 08:37:49 -0300

    Calculadora criada com os quatro operadores fundamentais

---
+++ Calculator/Controllers/CalculadoraController.cs
@@ -0,0 +1,44 @@
+﻿using Calculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Calculator.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index(CalculadoraViewModel calculadoraViewModel)
+        {
+            ModelState.Clear();
+            if (calculadoraViewModel.Operador.Equals("+"))
+            {
+                calculadoraViewModel.Resultado = calculadoraViewModel.Num1 + calculadoraViewModel.Num2;
+            }
+            else
+            if (calculadoraViewModel.Operador.Equals("-"))
+            {
+                calculadoraViewModel.Resultado = calculadoraViewModel.Num1 - calculadoraViewModel.Num2;
+            }
+            else
+            if (calculadoraViewModel.Operador.Equals("*"))
+            {
+                calculadoraViewModel.Resultado = calculadoraViewModel.Num1 * calculadoraViewModel.Num2;
+            }
+            else    
+            if (calculadoraViewModel.Operador.Equals("/"))
+            {
+                calculadoraViewModel.Resultado = calculadoraViewModel.Num1 / calculadoraViewModel.Num2;
+            }
+            
+            return View(calculadoraViewModel);
+        }
+    }
+}
